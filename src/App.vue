e75207899f193bc423aca14dc34a1e2e13d2318b
Pakin Tangsatiankij <177916027+Payya007@users.noreply.github.com.>
2026-05-12 15:50:09 +0700

    add contact pages

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
-<script setup></script>
+<script setup>
+import { RouterView } from 'vue-router'
+import { Toaster } from 'sonner'
+
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <RouterView />
+  <Toaster />
 </template>
 
 <style scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/contact',
+      name: 'Contact',
+      component: () => import('../pages/contact.vue'),
+    },
+    
+  ],
 })
 
 export default router
